29bb34461ccbfaf7cadb576886020e31975a04e7
Vasiliy <coder.vz@gmail.com>
2019-11-10 14:38:50 +0300

    added slim

---
+++ app/routes.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+use Slim\Views\PhpRenderer;
+use App\Application\Actions\User\ListUsersAction;
+use App\Application\Actions\User\ViewUserAction;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+
+return function (App $app) {
+    $app->get('/', function (Request $request, Response $response) {
+        $response->getBody()->write('Hello world!');
+        return $response;
+    });
+
+    $app->get('/hello/{name}', function ($request, $response, $args) {
+        $renderer = new PhpRenderer(__DIR__.'/../public/');
+        return $renderer->render($response, "hello.php", $args);
+    });
+
+    $app->group('/users', function (Group $group) {
+        $group->get('', ListUsersAction::class);
+        $group->get('/{id}', ViewUserAction::class);
+    });
+};
